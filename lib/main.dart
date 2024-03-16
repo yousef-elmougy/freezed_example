d3c04630ed268b9554c5fc306aa401b2a1dcc218
yousef <yousef.elmouyg@gmail.com>
2024-03-16 04:43:06 +0200

    Refactor MyHomePage constructor in main.dart Remove 'title' parameter from MyHomePage constructor and update references.

--- lib/main.dart
+++ lib/main.dart
@@ -17,14 +17,12 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: const MyHomePage(title: 'Flutter Demo Home Page'),
+        home: const MyHomePage(),
       );
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -39,7 +37,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text(widget.title),
+          title: const Text('Flutter Demo Home Page'),
         ),
         body: Center(
           child: Column(
